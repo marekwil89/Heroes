330b5f5fd68ab2b8bdb91394a03d7e5fbf086dd8
mrosochacka <magdalena.rosochacka94@gmail.com>
2016-11-12 21:15:21 +0100

    routes added

---
+++ public/app/app.js
@@ -0,0 +1,28 @@
+var app = angular.module("app", ['ngRoute'])
+
+
+app.config(function($routeProvider){
+
+	$routeProvider.when('/register', {
+		templateUrl: 'app/components//auth/register.html',
+		controller: 'registerCtrl'
+	});
+
+	$routeProvider.when('/login', {
+		templateUrl: 'app/components/auth/login.html',
+		controller: 'loginCtrl'
+	});
+
+	$routeProvider.otherwise({
+		redirectTo: '/'
+	});
+})
+
+
+app.controller('registerCtrl', function($scope){
+	$scope.user = 'main'
+})
+
+app.controller('loginCtrl', function($scope){
+	$scope.user = 'about'
+})
